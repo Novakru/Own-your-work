ab7775b110caefae3fcab8e22aa5c65407700628
jiangjiahao0208 <1748492421@qq.com>
2025-05-23 17:42:06 +0800

    try to fix tailwind css

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'Own-your-work';
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/myblogger' : '',
-}
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  trailingSlash: true,
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
